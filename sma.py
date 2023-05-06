c932020820d285aadd803a079dbb17ce1bc02f9f
warmachine <aashih.shetty98@gmail.com>
2023-05-06 17:20:24 +0530

    commit technical fun iteration 1

--- sma.py
+++ sma.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import talib
+import datetime
+
+current_time = datetime.datetime.now()
+
+curr_dt = current_time.strftime("%Y-%m-%d")
 def Aroon(df, period):
     high = df['High'].rolling(window=period+1).apply(np.argmax, raw=True)
     low = df['Low'].rolling(window=period+1).apply(np.argmin, raw=True)
@@ -10,9 +15,9 @@ def Aroon(df, period):
     return aroon_up, aroon_down
 
 # load data
-df = yf.download("TCS.NS", start="2023-01-01", end="2023-04-30", interval='1h')
+df = yf.download("RELIANCE.NS", start="2023-01-01", end=curr_dt, interval='1h')
 
-# calculate 200-day EMA
+# calculate 50-day EMA
 df['ema50'] = talib.EMA(df['Close'], timeperiod=50)
 
 # calculate Aroon indicator with period=14
@@ -26,14 +31,19 @@ df['signal'] = np.where((df['aroon_up'] > df['aroon_down']) & (df['Close'] > df[
 
 #df['signal'] = np.where((df['aroon_up'] < df['aroon_down']) & (df['Close'] < df['ema50']) & (df['rsi'] >= 40), 1,
 #                        np.where((df['aroon_up'] > df['aroon_down']) & (df['Close'] > df['ema50']) & (df['rsi'] <= 40), -1, 0))
-print(df['signal'])
 # print signals
 # plot signals
+for i in range(len(df)):
+    if df['signal'][i] == 1:
+        print("buy")
+    elif df['signal'][i] == 0:
+        print("sell")
+
 # plot signals
-#import matplotlib.pyplot as plt
-##fig, ax = plt.subplots(figsize=(16,8))
-#ax.plot(df.index, df['Close'])
-#ax.scatter(df[df['signal']==1].index, df[df['signal']==1]['Close'], color='red', label='Sell')
-#ax.scatter(df[df['signal']==-1].index, df[df['signal']==-1]['Close'], color='green', label='Buy')
-#plt.legend(loc='upper left')
-#plt.show()
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(16,8))
+ax.plot(df.index, df['Close'])
+ax.scatter(df[df['signal']==1].index, df[df['signal']==1]['Close'], color='red', label='Sell')
+ax.scatter(df[df['signal']==-1].index, df[df['signal']==-1]['Close'], color='green', label='Buy')
+plt.legend(loc='upper left')
+plt.show()
